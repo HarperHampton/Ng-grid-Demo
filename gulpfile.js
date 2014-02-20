b4070d13cbc046d29dcf8016e698dc0a9dc03bd9
leftstick <leftstick@qq.com>
2014-02-20 13:23:51 +0800

    fix bug that gulp cannot executed correctly

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var concat = require('gulp-concat');
 var template = require('gulp-template');
 var rename = require('gulp-rename');
 var connect = require('gulp-connect');
+var run = require('run-sequence');
 
 var deps = {
     dev: {
         js: ['bower_components/jquery/jquery.js', 'bower_components/underscore/underscore.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/angular/angular.js', 'bower_components/angular-route/angular-route.js', 'bower_components/ng-grid/ng-grid-2.0.7.debug.js'],
-        css: ['bower_components/bootstrap/dist/css/{bootstrap,bootstrap-theme}.css', 'bower_components/ng-grid/ng-grid.css', 'bower_components/jquery.ui/themes/base/jquery.ui.theme.css', 'src/css/*.css'],
+        css: ['bower_components/bootstrap/dist/css/{bootstrap.css,bootstrap-theme.css,bootstrap-theme.css.map,bootstrap.css.map}', 'bower_components/ng-grid/ng-grid.css', 'bower_components/jquery.ui/themes/base/jquery.ui.theme.css', 'src/css/*.css'],
         image: ['bower_components/jquery.ui/themes/base/images/*.*'],
         font: ['bower_components/bootstrap/dist/fonts/*.*']
     }
@@ -22,16 +23,16 @@ var libs = {
 };
 
 gulp.task('clean', function() {
-    return gulp.src('build', {
+    return gulp.src('build/**/*', {
         read: false
     }).pipe(clean());
 });
 
-gulp.task('concat', ['clean'], function() {
+gulp.task('concat', function() {
     return gulp.src(['src/js/CGrid.js', 'src/js/*.js']).pipe(concat("all.js")).pipe(gulp.dest('build/libs/'));
 });
 
-gulp.task('copy', ['concat'], function() {
+gulp.task('copy', function() {
     gulp.src(deps.dev.js).pipe(gulp.dest('build/libs/'));
     gulp.src(deps.dev.css).pipe(gulp.dest('build/css/'));
     gulp.src(deps.dev.image).pipe(gulp.dest('build/css/images/'));
@@ -39,7 +40,7 @@ gulp.task('copy', ['concat'], function() {
 });
 
 
-gulp.task('template', ['copy'], function() {
+gulp.task('template', function() {
     return gulp.src('src/**/*.html.tpl')
         .pipe(template({
             csss: libs.dev.css,
@@ -60,7 +61,9 @@ gulp.task('connect', ['template'], connect.server({
 }));
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.{js,css,html.tpl}', ['template']);
+    gulp.watch('src/**/*.{js,css,html.tpl}', ['concat', 'copy', 'template']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', function(cb) {
+    run('clean', ['concat', 'copy', 'template', 'connect', 'watch'], cb);
+});
